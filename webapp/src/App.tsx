b67d0ec6f481466a5c3a2038effec80f301f88d0
Jacob Poston <dev@jacobposton.com>
2023-11-04 14:23:15 -0400

    UI Overhaul

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,7 +1,25 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
-// import './App.css';
+
 import styles from './Home.module.css'
+
+import Button from '@mui/joy/Button';
+import Input from '@mui/joy/Input';
+import Select from '@mui/joy/Select';
+import Option from '@mui/joy/Option';
+import Divider from '@mui/joy/Divider';
+import Switch from '@mui/joy/Switch';
+import Chip from '@mui/joy/Chip';
+import Checkbox from '@mui/joy/Checkbox';
+
+import { FiAlertCircle, FiSlash } from "react-icons/fi";
+import { FiSearch } from "react-icons/fi";
+import { FiThumbsUp } from "react-icons/fi";
+import { FiThumbsDown } from "react-icons/fi";
+import { FiCheckCircle } from "react-icons/fi";
+import { Accordion, AccordionDetails, AccordionGroup, AccordionSummary, LinearProgress, Typography } from '@mui/joy';
+import { isDisabled } from '@testing-library/user-event/dist/utils';
+
 
 async function getWords(operators: WordOperation[]): Promise<WordPrediction[]> {
   const res = await fetch('/operations', {
@@ -47,9 +65,95 @@ const initialAppState = {
   predictions: []
 }
 
+
+const PredictionChip = (props: {displayText: string}) => {
+  const [checked, setChecked] = useState(false);
+
+  return (
+    <Chip
+      sx={{margin: "4px"}}
+      key={props.displayText}
+      variant="plain"
+      color={checked ? 'primary' : 'neutral'}
+      startDecorator={
+        checked && <FiCheckCircle style={{ zIndex: 1, pointerEvents: 'none' }} />
+      }
+    >
+      <Checkbox
+        variant="outlined"
+        color={checked ? 'primary' : 'neutral'}
+        disableIcon
+        overlay
+        label={props.displayText}
+        checked={checked}
+        onChange={(event) => {
+          // setSelected((names) =>
+          //   !event.target.checked
+          //     ? names.filter((n) => n !== name)
+          //     : [...names, name],
+          // );
+        }}
+      />
+    </Chip>
+  )
+}
+
+const OperatorInput = (props: {index: number, onSubmit: any, operation: WordOperation, updateOperator:any, disabled: boolean}) => {
+  const opFunctionBool = props.operation.function != "less_like";
+  
+  const placeholder = {
+    "more_like": "Words more like ...",
+    "less_like": "Words less like ...",
+    "start": "I'm thinking of a word like..."
+  }[props.operation.function]
+
+  return (
+    <Input
+      required={true}
+      value={props.operation.description}
+      disabled={props.disabled}
+      onChange={(e) => props.updateOperator(props.index, 'description', e.target.value)}
+      placeholder={placeholder}
+      startDecorator={(<React.Fragment>
+          {props.disabled ? (
+            opFunctionBool ? (
+              <FiThumbsUp style={{ color: '#0B6BCB' }} />
+            ) : (
+              <FiThumbsDown style={{ color: '#A51818' }} />
+            )
+          ) : (
+            <Switch
+              disabled={props.disabled}
+              color={opFunctionBool ?   'primary' : 'danger'}
+              startDecorator={
+                <FiThumbsDown
+                  style={{ color: opFunctionBool ? '#555E68' : '#A51818' }}
+                />
+              }
+              endDecorator={
+                <FiThumbsUp style={{ color: opFunctionBool ? '#0B6BCB' : '#555E68' }} />
+              }
+              checked={opFunctionBool}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                props.updateOperator(props.index, 'function', event.target.checked ? 'more_like' : 'less_like')
+              }
+            />
+          )}
+          <Divider sx={{marginLeft: "10px"}} orientation="vertical" />
+        </React.Fragment>
+        )
+      }
+      // endDecorator={!props.disabled && (<FiSearch></FiSearch>)}
+      // endDecorator={!props.disabled && (<Button onClick={props.onSubmit}><FiSearch></FiSearch></Button>)}
+    />
+  )
+}
+
+
 function App() {
   const [operators, setOperators] = useState<WordOperation[]>(clone(initialAppState.ops));
-  const [predictions, setPredictions] = useState<WordPrediction[]>(clone(initialAppState.predictions));
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const handleAddOperator = () => {
     setOperators([...operators, { description: '', function: 'more_like' }]);
@@ -59,19 +163,25 @@ function App() {
     const updatedOperators = [...operators];
     updatedOperators[index][field] = value;
     setOperators(updatedOperators);
-    // calculateWords(updatedOperators);
   };
 
   const handleRemoveOperator = (index: number) => {
     const updatedOperators = [...operators];
     updatedOperators.splice(index, 1);
     setOperators(updatedOperators);
-    // calculateWords(updatedOperators);
   };
+
+  const resetApp = () => {
+    setOperators(clone(initialAppState.ops));
+  }
 
   const calculateWords = (operators: WordOperation[]) => {
     operators = operators.filter(operator => operator.description !== "" && operator.description !== null);
     
+    setLoading(true);
+    setError(false);
+
+
     getWords(operators).then((latest_results: WordPrediction[]) => {
       console.log("Got results: ", latest_results);
       
@@ -82,81 +192,76 @@ function App() {
 
       // setPredictions(latest_results);
       handleAddOperator();
-    }).catch(e => console.log)
+    }).catch(e => {
+      console.log(e);
+      setError(true); 
+    }).finally(() => setLoading(false))
   };
 
-  console.log("Predictions: ", predictions);
+  // Debug:
+  console.log("Loading: ", loading);
   console.log("Operators: ", operators);
-
-  function resetApp(): void {
-    console.log(initialAppState)
-    setOperators(clone(initialAppState.ops));
-    setPredictions(clone(initialAppState.predictions));
-  }
 
   return (
     <div className={styles.container}>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Tip of My Tongue
-        </h1>
+        <Typography level="h1" fontWeight={"md"} color="neutral" >
+          TipofMyTongue.ai
+        </Typography>
 
-        <p className={styles.description}>
+        <Typography level="h4" fontWeight={"sm"} color="neutral" padding={"10px"} sx={{textAlign: "center"}}>
           It's 2023... why are we still using a thesaurus?
-        </p>
+        </Typography>
 
         <div className={styles.card}>
-            <h2>I'm thinking of a word like...</h2>
 
-            {operators.map((operator, index) => (
-              <div key={index} className={styles.operator}>
-                  {/* Word Function Operator Dropdown */}
-                  {index > 0 && (
-                    <select
-                    value={operator.function}
-                    onChange={(e) => handleOperatorChange(index, 'function', e.target.value as 'more_like' | 'less_like')}
-                  >
-                    <option value="more_like">More like</option>
-                    <option value="less_like">Less like</option>
-                  </select>
-                  )}
+          <form style={{display: "flex", flexDirection: "column", alignItems: "center"}} onSubmit={(event) => {
+            event.preventDefault();
+            calculateWords(operators);
+          }}>
 
-                  
-                <div className={styles.operatorInputs}>
-                  <input
-                    type="text"
-                    value={operator.description}
-                    onChange={(e) => handleOperatorChange(index, 'description', e.target.value)}
-                  />
+            <AccordionGroup size="sm" disableDivider>
+              {operators.map((operator, index) => (
+                <Accordion 
+                  key={index} 
+                  className={styles.operator} 
+                  expanded={index == operators.length - 2}>
 
-                  {/* Remove Operator Button */}
-                  {index > 0 && (
-                    <button onClick={() => handleRemoveOperator(index)}>X</button>
-                  )}
-                </div>
+                  <AccordionSummary className={styles.opGroup}>
+                    <OperatorInput 
+                      index={index}
+                      operation={operator}
+                      updateOperator={handleOperatorChange}
+                      disabled={index != operators.length - 1}
+                      onSubmit={() => { calculateWords(operators) }}></OperatorInput>
+                  </AccordionSummary>
 
-
-                {/* Results */}
-                {operator.results && (
-                <div className={styles.operatorResults}>
-                  {operator.results.map((prediction, index) => (
-                    <button key={index}>
-                      <span>{prediction.word}</span>
-                    </button>
-                  ))}
-                </div> 
-                )}
+                  {/* Results */}
+                  <AccordionDetails>
+                    {operator.results && (
+                      <div className={styles.operatorResults}>
+                        {operator.results.map((prediction, index) => (
+                          <PredictionChip key={prediction.word} displayText={prediction.word}></PredictionChip>
+                          ))}
+                      </div> 
+                    )}
+                </AccordionDetails>
+                
+                </Accordion>
+              ))}
+              </AccordionGroup>
+            
+              <div style={{width: "50%", padding: "20px"}}>
+                {loading && (<LinearProgress color='primary'/>)}
               </div>
-            ))}
 
-            <div className={styles.searchControls}>
-              {/* <button onClick={handleAddOperator}>Add Operator</button> */}
-              <button onClick={() => { calculateWords(operators) }}>Get Words</button>
-              
-              {/* Hide the reset button until an operator is provided */}
-              {operators.length > 1 && (<button onClick={() => resetApp()}>Start Over</button>)}
-            </div>
+              <div className={styles.searchControls}>
+                {/* Hide the reset button until an operator is provided */}
+                <Button type="submit" endDecorator={(<FiSearch></FiSearch>)}>Search</Button>
+                {operators.length > 1 && (<Button color='danger' variant='outlined' onClick={() => resetApp()} endDecorator={(<FiSlash></FiSlash>)}>Start Over</Button>)}
+              </div>
+            </form>
         </div>
       </main>
 
@@ -168,3 +273,18 @@ function App() {
 }
 
 export default App;
+
+{/* <Select
+variant="plain"
+value={"more_like"}
+onChange={(_, value) => { console.log(value);}}
+slotProps={{
+listbox: {
+variant: 'outlined',
+},
+}}
+sx={{ mr: -1.5, '&:hover': { bgcolor: 'transparent' } }}
+>
+<Option value="more_like">More Like</Option>
+<Option value="less_like">Less Like</Option>
+</Select> */}
