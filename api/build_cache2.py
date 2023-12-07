ed2570511c16c45e20b8873a21b509691f3464a0
Russell Land <rl02898@georgiasouthern.edu>
2023-12-07 02:32:16 -0500

    fixx batch size issue with last iteration, triton request and batch size must match

--- api/build_cache2.py
+++ api/build_cache2.py
@@ -1,44 +1,35 @@
-import pymilvus
 import math
-import torch
 import numpy as np
 
+from tqdm import tqdm
 from pymilvus import (
     connections,
     utility,
     FieldSchema,
     CollectionSchema,
     DataType,
-    Collection
+    Collection,
+    MilvusException
 )
 
 from app.embedding_utils import TritonRemoteModel
 
+try:
+    connections.connect(alias="default", host="localhost", port="19530")
+    model = TritonRemoteModel("http://localhost:8100", "gte-large")
+except MilvusException as e:
+    print(f"Could not establish connection to Milvus: {e}")
+except ConnectionRefusedError as e:
+    print(f"Could not establish connection to Triton: {e}")
 
-def save_embeddings(file_path, embeddings):
-    np_embeddings = embeddings
-
-    # save the embeddings array to the .npz file using a keyword argument
-    np.savez(file_path, embeddings=np_embeddings)
-    del(np_embeddings)
-
-def count_populated(a):
-    for i, line in enumerate(a):
-        if line.nonzero()[0].size == 0 or np.any(np.isnan(line)):
-            return i
-
-connections.connect(alias="default", host="localhost", port="19530")
-
-dim = 1024
+embedding_dims = 1024
 batch_size = 16
 collection_name = 'tiptest'
 
-collection_exists = utility.has_collection(collection_name)
-
-if not collection_exists:
+if not utility.has_collection(collection_name):
     fields = [
         FieldSchema(name="word", dtype=DataType.VARCHAR, is_primary=True, max_length=100),
-        FieldSchema(name="embedding", dtype=DataType.FLOAT_VECTOR, dim=dim)
+        FieldSchema(name="embedding", dtype=DataType.FLOAT_VECTOR, dim=embedding_dims)
     ]
     schema = CollectionSchema(fields=fields, description="Schema describing the Milvus collection, a vector store for the word embeddings.")
     tiptest = Collection(name="tiptest", schema=schema, using="default", consistency_level="Strong")
@@ -50,12 +41,23 @@ if not collection_exists:
     num_words = len(words)
     batches = math.ceil(num_words / batch_size)
 
-    model = TritonRemoteModel("http://localhost:8100", "gte-large")
-
-    for batch in range(0, batches, batch_size):
+    for batch in tqdm(
+        range(460000, num_words, batch_size),
+        desc=f"Batch number with batch size of {batch_size}",
+        ncols=150,
+        bar_format="{l_bar}{bar:10}{r_bar}"
+    ):
         text = words[batch:batch+batch_size]
 
-        model_output = model(np.array([str.encode(word) for word in text]))
+        if len(text) != batch_size:
+            last_text = text.copy()
+            L = batch_size - len(last_text)
+            for i in range(L):
+                last_text.append("filler")
+
+            model_output = model(np.array([str.encode(word) for word in last_text]))[:-L, :]
+        else:
+            model_output = model(np.array([str.encode(word) for word in text]))
 
         insert_result = tiptest.insert([
             text,
